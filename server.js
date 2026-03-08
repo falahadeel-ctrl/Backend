efd4d7ae931c05581f5517f6b32465bfa8d9b5e4
falahadeel-ctrl <falahadeel@gmail.com>
2026-03-08 16:07:50 -0400

    eeb

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const connectDB = async() =>{}
+const connectDB = async() =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('MongoDB connected')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+connectDB()
+
+app.listen(5000, () => console.log('Server running on port 5000'))
 
 mongoose.connect(process.env.MONGO_URL)
