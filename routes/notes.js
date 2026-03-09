ad0ba10fd9378548fb27b5538027386ccda01f7c
falahadeel-ctrl <falahadeel@gmail.com>
2026-03-08 20:59:04 -0400

    refbrf

---
+++ routes/notes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import Note from '../models/Note.js'
+
+const router = express.Router()
+
+//get all notes
+router.get('/', async (req, res)=>{
+    try {
+    const notes = await Note.find()
+    res.json(notes)
+    } catch (err){
+        res.status(500).json
+    }
+
+})
