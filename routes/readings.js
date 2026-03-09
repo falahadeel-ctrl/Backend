3481fd9b4740f0fe020e3bc8d6c6da25216768c1
falahadeel-ctrl <falahadeel@gmail.com>
2026-03-08 20:51:51 -0400

    rbre

--- routes/readings.js
+++ routes/readings.js
@@ -1,3 +1,36 @@
 import express from 'express'
 import Reading from '../models/Reading.js'
 
+const router = express.Router()
+
+//get all readings
+router.get('/', async (req, res)=>{
+    try {
+        const readings= await Reading.find()
+        res.json(readings)
+    } catch (err){
+        res.status(500).json({ message: err.message})
+    }
+})
+
+//post a new reading 
+router.post('/',async(req, res)=> {
+try{
+    const reading = await Reading.create(req.body)
+    res.json(reading)
+} catch (err){
+    res.status(500).json({ message: err.message})
+}
+})
+
+//delete a reading
+router.delete('/:id', async (req, res)=>{
+    try {
+        await Reading.findByIdAndDelete(req.params.id)
+        res.json({message: 'Reading deleted'})
+    } catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+export default router
